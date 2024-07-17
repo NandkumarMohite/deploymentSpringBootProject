9d7d1d92832616e194b366299797d9805b3cc2f0
NandkumarMohite <n.b.mohite88@gmail.com>
2024-07-17 20:22:19 +0530

    Change od origin

--- src/main/java/com/example/demo/controller/AttendenceController.java
+++ src/main/java/com/example/demo/controller/AttendenceController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin("https://44.203.138.109:8080")
 public class AttendenceController {
 
 
